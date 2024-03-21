cbbed7ab44457c3b2052109f0c97ecf7f75e9f7a
MoiPad92 <moipad85@gmail.com>
2024-03-20 22:46:50 -0600

    configuración de las vistas(C#)

--- ExamenApp2/Views/ProveedorFormPage.xaml.cs
+++ ExamenApp2/Views/ProveedorFormPage.xaml.cs
@@ -1,9 +1,22 @@
+using ExamenApp2.Models;
+using ExamenApp2.ViewModels;
+
 namespace ExamenApp2.Views;
 
 public partial class ProveedorFormPage : ContentPage
 {
+	private ProveedorFormPageViewModel viewModel;
 	public ProveedorFormPage()
 	{
 		InitializeComponent();
+		viewModel = new ProveedorFormPageViewModel();
+		this.BindingContext = viewModel;
 	}
+
+	public ProveedorFormPage(Proveedor proveedor)
+	{
+        InitializeComponent();
+		viewModel = new ProveedorFormPageViewModel(proveedor);
+		this.BindingContext = viewModel;
+    }
 }
--- ExamenApp2/Views/ProveedorMainPage.xaml.cs
+++ ExamenApp2/Views/ProveedorMainPage.xaml.cs
@@ -1,9 +1,20 @@
+using ExamenApp2.ViewModels;
+
 namespace ExamenApp2.Views;
 
 public partial class ProveedorMainPage : ContentPage
 {
+	private ProveedorMainPageViewModel viewModel;
 	public ProveedorMainPage()
 	{
 		InitializeComponent();
+		viewModel = new ProveedorMainPageViewModel();
+		this.BindingContext = viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		viewModel.GetAll();
+    }
 }
