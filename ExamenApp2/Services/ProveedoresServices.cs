f04ecc167a3f53b0b51eb3e3f066b369b33ac3dd
MoiPad92 <moipad85@gmail.com>
2024-03-20 22:05:01 -0600

    Actualización de Servicios

--- ExamenApp2/Services/ProveedoresServices.cs
+++ ExamenApp2/Services/ProveedoresServices.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using ExamenApp2.Models;
+using SQLite;
 
 namespace ExamenApp2.Services
 {
-    internal class ProveedoresServices
+    public class ProveedoresServices
     {
+        private readonly SQLiteConnection _dbConnection;
+
+        public ProveedoresServices()
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Proveedor.db3");
+            _dbConnection = new SQLiteConnection(dbPath);
+            _dbConnection.CreateTable<Proveedor>();
+        }
+
+        public List<Proveedor> GetAll()
+        {
+            return _dbConnection.Table<Proveedor>().ToList();
+        }
+
+        public int Insert(Proveedor proveedor)
+        {
+            return _dbConnection.Insert(proveedor);
+        }
+
+        public int Update(Proveedor proveedor)
+        {
+            return _dbConnection.Update(proveedor);
+        }
+
+        public int Delete(Proveedor proveedor)
+        {
+            return _dbConnection.Delete(proveedor);
+        }
     }
 }
