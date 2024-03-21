78b2db9fab26b24fa64ddca88f90d68e04786d5f
MoiPad92 <moipad85@gmail.com>
2024-03-20 21:48:51 -0600

    Vistas del modelo

---
+++ ExamenApp2/ViewModels/ProveedorMainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenApp2.ViewModels
+{
+    internal class ProveedorMainPageViewModel
+    {
+    }
+}
