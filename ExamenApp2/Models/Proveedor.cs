9963a2770c77a74afcc8055a4a3bce9c54eb69bc
MoiPad92 <moipad85@gmail.com>
2024-03-20 21:48:15 -0600

    Modelos

---
+++ ExamenApp2/Models/Proveedor.cs
@@ -0,0 +1,22 @@
+﻿
+
+using SQLite;
+
+namespace ExamenApp2.Models
+{
+    internal class Proveedor
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Tipo { get; set; }
+
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Servicio { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
