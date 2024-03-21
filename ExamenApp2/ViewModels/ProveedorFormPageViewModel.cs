ce494358a706a4f5d2771962749facc68a188545
MoiPad92 <moipad85@gmail.com>
2024-03-20 22:46:27 -0600

    Configuración de las vistas modelos

--- ExamenApp2/ViewModels/ProveedorFormPageViewModel.cs
+++ ExamenApp2/ViewModels/ProveedorFormPageViewModel.cs
@@ -1,12 +1,144 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExamenApp2.Models;
+using ExamenApp2.Services;
+
 
 namespace ExamenApp2.ViewModels
 {
-    internal class ProveedorFormPageViewModel
+    public partial class ProveedorFormPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private int id;
+
+        [ObservableProperty]
+        private string nombre;
+
+        [ObservableProperty]
+        private string tipo;
+
+        [ObservableProperty]
+        private string telefono;
+
+        [ObservableProperty]
+        private string email;
+
+        [ObservableProperty]
+        private string servicio;
+
+        [ObservableProperty]
+        private string direccion;
+
+        private readonly ProveedoresServices proveedoresServices;
+
+        public ProveedorFormPageViewModel() 
+        {
+            proveedoresServices = new ProveedoresServices();
+        }
+
+        public ProveedorFormPageViewModel(Proveedor proveedor)
+        {
+            Id = proveedor.Id;
+            Nombre = proveedor.Nombre;
+            Tipo = proveedor.Tipo;
+            Telefono = proveedor.Telefono;
+            Email = proveedor.Email;
+            Servicio = proveedor.Servicio;
+            Direccion = proveedor.Direccion;
+
+            proveedoresServices = new ProveedoresServices();
+
+        }
+
+        [RelayCommand]
+        private async Task CreateUpdate()
+        {
+            try 
+            {
+                Proveedor proveedor = new Proveedor
+                {
+                    Id = id,
+                    Nombre = nombre,
+                    Tipo = tipo,
+                    Telefono = telefono,
+                    Email = email,
+                    Servicio = servicio,
+                    Direccion = direccion
+                };
+
+                if(Validar(proveedor))
+                {
+                    if(Id == 0)
+                    {
+                        proveedoresServices.Insert(proveedor);
+                    }
+                    else 
+                    {
+                        proveedoresServices.Update(proveedor);  
+                    }
+
+                    await App.Current!.MainPage!.Navigation.PopAsync();
+                }
+            
+            }catch (Exception ex) 
+            {
+                Alerta("Error!", $"Excepción: {ex.Message}");
+            }
+        }
+
+        private bool Validar(Proveedor proveedor)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(proveedor.Nombre))
+                {
+                    Alerta("Advertencia!","Los campos no pueder ir vacios, escriba el nombre");
+                    return false; 
+                }
+                else if (string.IsNullOrEmpty(proveedor.Tipo))
+                {
+                    Alerta("Advertencia!", "Los campos no pueder ir vacios, escriba el tipo de proveedor");
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(proveedor.Telefono))
+                {
+                    Alerta("Advertencia!", "Los campos no pueder ir vacios, escriba el telefono");
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(proveedor.Email))
+                {
+                    Alerta("Advertencia!", "Los campos no pueder ir vacios, escriba el correo");
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(proveedor.Servicio))
+                {
+                    Alerta("Advertencia!", "Los campos no pueder ir vacios, escriba el servicio");
+                    return false;
+                }
+                else if (string.IsNullOrEmpty(proveedor.Direccion))
+                {
+                    Alerta("Advertencia!", "Los campos no pueder ir vacios, escriba la dirección");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex) 
+            {
+                Alerta("Error", $"Excepción: {ex.Message}");
+                return false;
+            }
+        }
+
+        private void Alerta(string tipo, string mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(tipo, mensaje, "Aceptar"));
+        }
+
+
+
+
     }
 }
--- ExamenApp2/ViewModels/ProveedorMainPageViewModel.cs
+++ ExamenApp2/ViewModels/ProveedorMainPageViewModel.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ExamenApp2.Models;
+using ExamenApp2.Services;
+using ExamenApp2.Views;
+using System.Collections.ObjectModel;
 
 namespace ExamenApp2.ViewModels
 {
-    internal class ProveedorMainPageViewModel
+    public partial class ProveedorMainPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<Proveedor> proveedorCollection = new ObservableCollection<Proveedor>();
+
+        private readonly ProveedoresServices proveedoresServices;
+
+        public ProveedorMainPageViewModel()
+        {
+            proveedoresServices = new ProveedoresServices();
+        }
+
+        public void GetAll()
+        {
+            var getAll = proveedoresServices.GetAll();
+
+            if(getAll.Count > 0)
+            {
+                ProveedorCollection.Clear();
+
+                foreach (var proveedor in getAll)
+                {
+                    ProveedorCollection.Add(proveedor);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task SelectProveedor(Proveedor proveedor)
+        {
+            try
+            {
+                string res = await App.Current!.MainPage!.DisplayActionSheet("Operación","Cancelar", null, "Actualizar", "Eliminar");
+
+                switch(res)
+                {
+                    case "Actualizar":
+                        await App.Current.MainPage.Navigation.PushAsync(new ProveedorFormPage(proveedor));
+                        break;
+
+                    case "Eliminar":
+
+                        bool respuesta = await App.Current!.MainPage!.DisplayAlert("Eliminar Proveedor","¿Esta seguro de eliminar el proveedor?", "Si", "No");
+
+                        if(respuesta)
+                        {
+                            int del = proveedoresServices.Delete(proveedor);
+                            if(del > 0)
+                            {
+                                ProveedorCollection.Remove(proveedor);
+                            }
+                        }
+                        break;  
+                }
+
+            }catch (Exception ex) 
+            {
+                Alerta("Error!", $"Excepción{ex.Message}");
+            }
+        }
+
+        private void Alerta(string tipo, string mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(tipo, mensaje, "Aceptar"));
+        }
     }
 }
